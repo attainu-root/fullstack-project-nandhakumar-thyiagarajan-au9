6b2bb0303edcb9cd0385a2695a7c27a38159ebbc
nandhakumar-thyiagarajan-au9 <nandhakumar19984@gmail.com>
2021-03-11 07:39:20 +0530

    added updates

--- client/my-app/src/Components/Login.js
+++ client/my-app/src/Components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
 import './Login.css';
 
 class Login extends React.Component {
@@ -12,6 +13,8 @@ class Login extends React.Component {
       Email: '',
       Password: '',
       RepeatPassword: '',
+      alert: false,
+      alertMessage: '',
     };
   }
 
@@ -23,6 +26,8 @@ class Login extends React.Component {
       Email: '',
       Password: '',
       RepeatPassword: '',
+      alert: false,
+      alertMessage: '',
     }));
   };
 
@@ -33,11 +38,72 @@ class Login extends React.Component {
     });
   };
 
+  onsignup = () => {
+    if (this.state.Username.length <= 0) {
+      this.setState({
+        ...this.state,
+        alert: true,
+        alertMessage: 'ENTER USERNAME',
+      });
+    } else if (this.state.Email.length <= 10) {
+      this.setState({
+        ...this.state,
+        alert: true,
+        alertMessage: 'ENTER VALID EMAIL',
+      });
+    } else if (this.state.Password.length <= 5) {
+      this.setState({
+        ...this.state,
+        alert: true,
+        alertMessage: 'ENTER STRONG PASSWORD',
+      });
+    } else if (this.state.Password !== this.state.RepeatPassword) {
+      this.setState({
+        ...this.state,
+        alert: true,
+        alertMessage: 'BOTH PASSWORD MISMATCH',
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        alert: false,
+        alertMessage: '',
+      });
+    }
+  };
+
+  onlogin = () => {
+    if (this.state.Email.length <= 10) {
+      this.setState({
+        ...this.state,
+        alert: true,
+        alertMessage: 'ENTER VALID EMAIL',
+      });
+    } else if (this.state.Password.length <= 5) {
+      this.setState({
+        ...this.state,
+        alert: true,
+        alertMessage: 'ENTER VALID PASSWORD',
+      });
+    } else {
+      this.setState({
+        ...this.state,
+        alert: false,
+        alertMessage: '',
+      });
+    }
+  };
+
   render() {
     return (
       <React.Fragment>
         <section className="auth_section">
           <form className="auth_form">
+            {this.state.alert && (
+              <>
+                <Alert severity="error">{this.state.alertMessage}</Alert>
+              </>
+            )}
             {this.state.signup && (
               <>
                 <TextField
@@ -85,6 +151,7 @@ class Login extends React.Component {
                   className="primary_inputs"
                   variant="contained"
                   color="primary"
+                  onClick={this.onsignup}
                 >
                   SIGN UP
                 </Button>
@@ -95,6 +162,7 @@ class Login extends React.Component {
                   className="primary_inputs"
                   variant="contained"
                   color="primary"
+                  onClick={this.onlogin}
                 >
                   LOGIN
                 </Button>
