9aa1e9a2c20aba8853ff89df7a9a09eb8faadabb
nandhakumar-thyiagarajan-au9 <nandhakumar19984@gmail.com>
2021-03-16 08:13:46 +0530

    added new updates

---
+++ client/my-app/src/Components/ProtectedRoute.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const ProtectedRoute = ({ component: Component, ...rest }) => {
+  return (
+    <>
+      <Route
+        {...rest}
+        render={(props) => {
+          if (document.cookie) {
+            return <Component {...props} />;
+          } else {
+            return (
+              <Redirect
+                to={{ pathname: '/', state: { from: props.location } }}
+              />
+            );
+          }
+        }}
+      />
+    </>
+  );
+};
+
+export default ProtectedRoute;
--- client/my-app/src/Router/Routes.js
+++ client/my-app/src/Router/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from '../Components/Login';
 import Homepage from '../Components/Homepage';
+import ProtectedRoute from '../Components/ProtectedRoute';
 
 const Router = () => {
   return (
@@ -9,7 +10,9 @@ const Router = () => {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Login} />
-          <Route path="/homepage" exact component={Homepage} />
+          <Route path="/homepage" exact>
+            <ProtectedRoute component={Homepage} />
+          </Route>
         </Switch>
       </BrowserRouter>
     </>
